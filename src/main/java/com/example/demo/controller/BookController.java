b1ecaabc6875bdee64c3b37107ec6ef21c224883
Harshita Goyal <ray@Harshitas-MacBook-Air-2.local>
2026-06-09 22:07:31 +0530

    Implemented Add Book and List Books

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
+
+import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
@@ -14,10 +20,21 @@ public class BookController {
 
      @GetMapping("/")
     public String home(Model model){
-
         model.addAttribute("books", brp.findAll());
-
         return "index";
+    }
+
+
+    @GetMapping("/add")
+    public String addBookPage(Model model) {
+    model.addAttribute("book", new Book());
+    return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(@ModelAttribute Book book) {
+    brp.save(book);
+    return "redirect:/";
     }
     
 }
